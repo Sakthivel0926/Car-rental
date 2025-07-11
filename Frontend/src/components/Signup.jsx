0537cbaa3b4519ff5f9633e5d7f3b232fb635f19
Sakthivel0926 <sakthivel.c2022cse@sece.ac.in>
2025-07-11 09:58:08 +0530

    commited

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-<<<<<<< HEAD
+
 import { Car, Mail, Lock, User, Eye, EyeOff, phone} from "lucide-react";
-=======
+
 import { Car, Mail, Lock, User, Eye, EyeOff, Phone } from "lucide-react";
->>>>>>> ec7ce9f (deployment link is added)
+
 
 const Signup = ({ onSignup }) => {
   const navigate = useNavigate();
@@ -125,11 +125,11 @@ const Signup = ({ onSignup }) => {
             <div className="relative">
               <label className="block text-sm font-medium text-blue-200 mb-2">Phone Number</label>
               <div className="relative">
-<<<<<<< HEAD
+
                 <phone size={20} className="absolute left-3 top-1/2 transform -translate-y-1/2 text-blue-300" />
-=======
+
                 <Phone size={20} className="absolute left-3 top-1/2 transform -translate-y-1/2 text-blue-300" />
->>>>>>> ec7ce9f (deployment link is added)
+
                 <input
                   name="phone"
                   type="text"
