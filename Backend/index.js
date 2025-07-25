ad006d1e5cbf8e7c946dd68f324947083b8c83ed
Sakthivel0926 <sakthivel.c2022cse@sece.ac.in>
2025-07-25 09:22:35 +0530

    cors error rectified

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const bookingRoutes = require('./routes/bookingRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173'];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173','*'];
 app.use(cors({
   origin: function(origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
